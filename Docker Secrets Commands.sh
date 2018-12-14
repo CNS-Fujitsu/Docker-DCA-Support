d195ea447e75a454888a45b2b0753c9392a21472
sowens81 <steve.owens@uk.fujitsu.com>
2018-12-14 13:15:43 +0000

    update

--- Docker Secrets Commands.sh
+++ Docker Secrets Commands.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-#Get the client version and engine version of Docker
-docker version
+#Create Secret from File
+docker secret create {secretname} {filename}
 
-#Get the configuration information for a docker engine
-docker info
+#Create Secret from Command Line
+echo "{Password}" | docker secret create {secretname} -
 
-#Get a list of all the main docker commands
-docker
+#Display all Secrets
+docker secret ls
 
-#Get a list of all the docker management commands
-docker container
-docker config
-docker image
+#Inspect a Secret
+Docker secret inspect {secretname}
 
+#Map a secret to a service
+Docker service create --name my_sql --secret {secretname}
 
+#Secret Example - Create Postgres Service
+echo "username" | docker secret create psql_user
+echo "password" | docker secret create psql_pass
+Docker service create --name mysql1 --secret mysql_user --secret mysql_pass -e POSTGRES_PASSWORD_FILE=/run/secrets/psql_pass -e POSTGRES_USER_FILE=/run/secrets/psql_user postgres
 
-
-
-
-
+#Replace Secrets
 
 
